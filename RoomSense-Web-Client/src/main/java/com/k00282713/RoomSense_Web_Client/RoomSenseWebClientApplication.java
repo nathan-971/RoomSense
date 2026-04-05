622d7d8ba8b8f69afbd50b3e105c9e72d6422d83
nathan-971 <nathanberry971@gmail.com>
2026-04-05 22:22:19 +0100

    Inital Commit, Project Structure Setup

---
+++ RoomSense-Web-Client/src/main/java/com/k00282713/RoomSense_Web_Client/RoomSenseWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.k00282713.RoomSense_Web_Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomSenseWebClientApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RoomSenseWebClientApplication.class, args);
+	}
+}
---
+++ RoomSense-Web-Client/src/test/java/com/k00282713/RoomSense_Web_Client/RoomSenseWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k00282713.RoomSense_Web_Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomSenseWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
